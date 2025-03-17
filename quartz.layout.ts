37518e6b98ce59790045028ddb206385a72ac739
Evan Fosmark <evanfosmark@microsoft.com>
2025-03-17 12:49:13 -0700

    Quartz sync: Mar 17, 2025, 12:49 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,7 +36,8 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer(),
     Component.DesktopOnly(Component.TableOfContents())
-  ]
+  ],
+  right: [],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
